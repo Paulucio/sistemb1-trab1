caf31e4dbbb92594670c144bb808fc02840a8ad0
paulucio <leonardo.paulucio@hotmail.com>
2016-05-18 19:31:45 -0300

    Histograma em construcao

--- trab.asm
+++ trab.asm
@@ -27,6 +27,35 @@ call LE_ARQUIVO
 
 call IMPRIME_IMG
 
+call HISTOG
+
+mov dx, 255
+push dx
+mov dx, 240
+push dx
+mov dx, 255
+push dx
+mov dx, 639
+push dx
+
+call line
+
+mov dx, 510
+push dx
+mov dx, 240
+push dx
+mov dx, 510
+push dx
+mov dx, 639
+push dx
+
+call line
+
+;call PRINTA_HIST
+
+mov bx, HISTOGRAMA
+mov cx, f
+int 3
 mov ah,08h
 int 21h
 mov ah,0   			; set video mode
@@ -402,9 +431,7 @@ IMPRIME_IMG:
 			push dx ;posx
 			push ax	;posy
 			mov al, byte[DADOS+si]
-			and ax, 00FFh
-			mov bh, 10h  ; 16 para dividir
-			div bh
+			shr al, 4 ; dividindo por 16 shift para a direita de 4 bits
 			mov byte[cor], al
 
 			call plot_xy
@@ -423,12 +450,46 @@ ret
 	
 ; ----------------------------- HISTOGRAMA --------------------------------
 
-HISTOGRAMA:
+HISTOG:     ;lembrar de colocar constante em 62500
+	xor si, si
+	xor di, di
+	xor ax, ax
+	mov cx, 62500
+	hist:	
+		push cx
+
+		mov al, byte[DADOS+si]
+		inc si
 	
+		mov di, ax
+		inc word[HISTOGRAMA + di + 1]
+		pop cx
+	loop hist
+ret
 
+PRINTA_HIST:
+	mov dx, 251 ; posx inicial
+	xor si,si
 
+	mov cx, 255
+	p_hist:
+		mov ax, 240 ;posy inicial
+		push dx 
+		push dx ; x1
+		push ax	; y1
+		push dx ; x2
 
-
+		mov bx, [HISTOGRAMA + si]
+		add ax, bx
+		push ax ;y2
+		
+		call line		
+		
+		pop dx
+		inc dx
+		add si,2
+	loop p_hist
+ret
 ;***************************************************************************
 ;
 ;   fun??o cursor
@@ -517,279 +578,6 @@ plot_xy:
 	pop		bp
 	ret		4
 ;_____________________________________________________________________________
-;    fun??o circle
-;	 push xc; push yc; push r; call circle;  (xc+r<639,yc+r<479)e(xc-r>0,yc-r>0)
-; cor definida na variavel cor
-circle:
-	push 	bp
-	mov	 	bp,sp
-	pushf                        ;coloca os flags na pilha
-	push 	ax
-	push 	bx
-	push	cx
-	push	dx
-	push	si
-	push	di
-
-	mov		ax,[bp+8]    ; resgata xc
-	mov		bx,[bp+6]    ; resgata yc
-	mov		cx,[bp+4]    ; resgata r
-
-	mov 	dx,bx
-	add		dx,cx       ;ponto extremo superior
-	push    ax
-	push	dx
-	call plot_xy
-
-	mov		dx,bx
-	sub		dx,cx       ;ponto extremo inferior
-	push    ax
-	push	dx
-	call plot_xy
-
-	mov 	dx,ax
-	add		dx,cx       ;ponto extremo direita
-	push    dx
-	push	bx
-	call plot_xy
-
-	mov		dx,ax
-	sub		dx,cx       ;ponto extremo esquerda
-	push    dx
-	push	bx
-	call plot_xy
-
-	mov		di,cx
-	sub		di,1	 ;di=r-1
-	mov		dx,0  	;dx ser? a vari?vel x. cx ? a variavel y
-
-	;aqui em cima a l?gica foi invertida, 1-r => r-1
-	;e as compara??es passaram a ser jl => jg, assim garante
-	;valores positivos para d
-
-stay:				;loop
-	mov		si,di
-	cmp		si,0
-	jg		inf       ;caso d for menor que 0, seleciona pixel superior (n?o  salta)
-	mov		si,dx		;o jl ? importante porque trata-se de conta com sinal
-	sal		si,1		;multiplica por doi (shift arithmetic left)
-	add		si,3
-	add		di,si     ;nesse ponto d=d+2*dx+3
-	inc		dx		;incrementa dx
-	jmp		plotar
-inf:
-	mov		si,dx
-	sub		si,cx  		;faz x - y (dx-cx), e salva em di
-	sal		si,1
-	add		si,5
-	add		di,si		;nesse ponto d=d+2*(dx-cx)+5
-	inc		dx		;incrementa x (dx)
-	dec		cx		;decrementa y (cx)
-
-plotar:
-	mov		si,dx
-	add		si,ax
-	push    si			;coloca a abcisa x+xc na pilha
-	mov		si,cx
-	add		si,bx
-	push    si			;coloca a ordenada y+yc na pilha
-	call plot_xy		;toma conta do segundo octante
-	mov		si,ax
-	add		si,dx
-	push    si			;coloca a abcisa xc+x na pilha
-	mov		si,bx
-	sub		si,cx
-	push    si			;coloca a ordenada yc-y na pilha
-	call plot_xy		;toma conta do s?timo octante
-	mov		si,ax
-	add		si,cx
-	push    si			;coloca a abcisa xc+y na pilha
-	mov		si,bx
-	add		si,dx
-	push    si			;coloca a ordenada yc+x na pilha
-	call plot_xy		;toma conta do segundo octante
-	mov		si,ax
-	add		si,cx
-	push    si			;coloca a abcisa xc+y na pilha
-	mov		si,bx
-	sub		si,dx
-	push    si			;coloca a ordenada yc-x na pilha
-	call plot_xy		;toma conta do oitavo octante
-	mov		si,ax
-	sub		si,dx
-	push    si			;coloca a abcisa xc-x na pilha
-	mov		si,bx
-	add		si,cx
-	push    si			;coloca a ordenada yc+y na pilha
-	call plot_xy		;toma conta do terceiro octante
-	mov		si,ax
-	sub		si,dx
-	push    si			;coloca a abcisa xc-x na pilha
-	mov		si,bx
-	sub		si,cx
-	push    si			;coloca a ordenada yc-y na pilha
-	call plot_xy		;toma conta do sexto octante
-	mov		si,ax
-	sub		si,cx
-	push    si			;coloca a abcisa xc-y na pilha
-	mov		si,bx
-	sub		si,dx
-	push    si			;coloca a ordenada yc-x na pilha
-	call plot_xy		;toma conta do quinto octante
-	mov		si,ax
-	sub		si,cx
-	push    si			;coloca a abcisa xc-y na pilha
-	mov		si,bx
-	add		si,dx
-	push    si			;coloca a ordenada yc-x na pilha
-	call plot_xy		;toma conta do quarto octante
-
-	cmp		cx,dx
-	jb		fim_circle  ;se cx (y) est? abaixo de dx (x), termina
-	jmp		stay		;se cx (y) est? acima de dx (x), continua no loop
-
-
-fim_circle:
-	pop		di
-	pop		si
-	pop		dx
-	pop		cx
-	pop		bx
-	pop		ax
-	popf
-	pop		bp
-	ret		6
-;-----------------------------------------------------------------------------
-;    fun??o full_circle
-;	 push xc; push yc; push r; call full_circle;  (xc+r<639,yc+r<479)e(xc-r>0,yc-r>0)
-; cor definida na variavel cor
-full_circle:
-	push 	bp
-	mov	 	bp,sp
-	pushf                        ;coloca os flags na pilha
-	push 	ax
-	push 	bx
-	push	cx
-	push	dx
-	push	si
-	push	di
-
-	mov		ax,[bp+8]    ; resgata xc
-	mov		bx,[bp+6]    ; resgata yc
-	mov		cx,[bp+4]    ; resgata r
-
-	mov		si,bx
-	sub		si,cx
-	push    ax			;coloca xc na pilha
-	push	si			;coloca yc-r na pilha
-	mov		si,bx
-	add		si,cx
-	push	ax		;coloca xc na pilha
-	push	si		;coloca yc+r na pilha
-	call line
-
-
-	mov		di,cx
-	sub		di,1	 ;di=r-1
-	mov		dx,0  	;dx ser? a vari?vel x. cx ? a variavel y
-
-;aqui em cima a l?gica foi invertida, 1-r => r-1
-;e as compara??es passaram a ser jl => jg, assim garante
-;valores positivos para d
-
-stay_full:				;loop
-	mov		si,di
-	cmp		si,0
-	jg		inf_full       ;caso d for menor que 0, seleciona pixel superior (n?o  salta)
-	mov		si,dx		;o jl ? importante porque trata-se de conta com sinal
-	sal		si,1		;multiplica por doi (shift arithmetic left)
-	add		si,3
-	add		di,si     ;nesse ponto d=d+2*dx+3
-	inc		dx		;incrementa dx
-	jmp		plotar_full
-	inf_full:
-	mov		si,dx
-	sub		si,cx  		;faz x - y (dx-cx), e salva em di
-	sal		si,1
-	add		si,5
-	add		di,si		;nesse ponto d=d+2*(dx-cx)+5
-	inc		dx		;incrementa x (dx)
-	dec		cx		;decrementa y (cx)
-
-plotar_full:
-	mov		si,ax
-	add		si,cx
-	push	si		;coloca a abcisa y+xc na pilha
-	mov		si,bx
-	sub		si,dx
-	push    si		;coloca a ordenada yc-x na pilha
-	mov		si,ax
-	add		si,cx
-	push	si		;coloca a abcisa y+xc na pilha
-	mov		si,bx
-	add		si,dx
-	push    si		;coloca a ordenada yc+x na pilha
-	call 	line
-
-	mov		si,ax
-	add		si,dx
-	push	si		;coloca a abcisa xc+x na pilha
-	mov		si,bx
-	sub		si,cx
-	push    si		;coloca a ordenada yc-y na pilha
-	mov		si,ax
-	add		si,dx
-	push	si		;coloca a abcisa xc+x na pilha
-	mov		si,bx
-	add		si,cx
-	push    si		;coloca a ordenada yc+y na pilha
-	call	line
-
-	mov		si,ax
-	sub		si,dx
-	push	si		;coloca a abcisa xc-x na pilha
-	mov		si,bx
-	sub		si,cx
-	push    si		;coloca a ordenada yc-y na pilha
-	mov		si,ax
-	sub		si,dx
-	push	si		;coloca a abcisa xc-x na pilha
-	mov		si,bx
-	add		si,cx
-	push    si		;coloca a ordenada yc+y na pilha
-	call	line
-
-	mov		si,ax
-	sub		si,cx
-	push	si		;coloca a abcisa xc-y na pilha
-	mov		si,bx
-	sub		si,dx
-	push    si		;coloca a ordenada yc-x na pilha
-	mov		si,ax
-	sub		si,cx
-	push	si		;coloca a abcisa xc-y na pilha
-	mov		si,bx
-	add		si,dx
-	push    si		;coloca a ordenada yc+x na pilha
-	call	line
-
-	cmp		cx,dx
-	jb		fim_full_circle  ;se cx (y) est? abaixo de dx (x), termina
-	jmp		stay_full		;se cx (y) est? acima de dx (x), continua no loop
-
-
-fim_full_circle:
-	pop		di
-	pop		si
-	pop		dx
-	pop		cx
-	pop		bx
-	pop		ax
-	popf
-	pop		bp
-	ret		6
-;-----------------------------------------------------------------------------
-;
 ;   fun??o line
 ;
 ; push x1; push y1; push x2; push y2; call line;  (x<639, y<479)
@@ -1017,6 +805,7 @@ RESULTADO 	db 	0, '$'
 N_PIX 		dw 	62500
 DADOS 		resb 	62500
 HISTOGRAMA	resw	255
+f db '34'
 
 
 
